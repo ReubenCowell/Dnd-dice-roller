dc111f8f63da1225d9abd9aa33103939184827e4
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2022-08-14 21:38:56 +0100

    Added comments and refactored some code

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def get_values(die):
     d_num = int(die[:d_loc])  # gets the number of dice
     die = die[d_loc + 1:]  # removes the number of dice from the rest of the string
 
+    # This works out if there is a + or - in the dice, a modifier, to change the result, if not, it just becomes 0
     if die.find("+") != -1:
         m = die[die.find("+"):]
         d_sides = die[:die.find("+")]
@@ -27,8 +28,6 @@ def get_values(die):
         m = 0
 
 
-
-
 while True:
     try:
         dice = str(input(
@@ -37,15 +36,15 @@ while True:
         if dice == "stop":
             print("Ending dice roller...")
             break
-        get_values(dice)
-        if int(d_sides) > 100 or d_sides < 2:
+        get_values(dice)  # this splits the users answer into data to calculate
+        # this below makes sure it is a valid entry:
+        if int(d_sides) > 100 or int(d_sides)< 2:
             print("Size of dice must be no more than 100 and less than 2")
             continue
         if int(d_num) > 100:
             print("The number of dice cant be more than 100")
             continue
-        output = dice_roll(d_num, d_sides, m)
-        string = ', '.join([str(x) for x in output[0]])
-        print(str(output[1]) + ": " + string)
-    except ValueError:
+        output = dice_roll(d_num, d_sides, m)  # gets the output of the dice roll
+        print(str(output[1]) + ": " + ', '.join([str(x) for x in output[0]]))  # prints to user in pleasing manner
+    except ValueError:  # catches any error if user enters random string for dice
         print("Dice entered incorrectly")
