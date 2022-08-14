255a369da04a3c2d523a435f4ed75370bd5035f5
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2022-08-14 13:54:35 +0100

    Re-making program

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-from random import randint
-import re
+from random import randint  # random numbers are used to create a dice roll
 
 
 def dice_roll(sides, dice):  # returns a list of rolled dice and the sum of that list
     rolled = []
     for _ in range(0, dice):
         rolled.append(randint(1, int(sides)))
-    #    total = sum(rolled)
-    return rolled, sum(rolled)
+    return rolled, sum(rolled)  # returns a list of the rolled dice and the total of them
 
 
-dice = input("Please input dice to be rolled")
-# splitted= dicerequested[1:].split('+')
-# num_of_dice = dicerequested[:1]
+dice = input("Please input dice to be rolled \n In format ?d? +/- ?")  # gets the dice that user wants to roll
 
-# sides_on_dice = int(splitted[0].split('+')[0]
-# print(dice_roll(num_of_dice, sides_on_dice))
-
-num_dice = dice[:1]
-dice = dice[1:]
-
-if '+' in dice:
-    dice = dice.split('+')
-    modifier = dice[1]
-    sides_dice = dice[0]
-elif '-' in dice:
-    dice = dice.split('-')
-    modifier = dice[1]
-    sides_dice = dice[0]
-else:
-    sides_dice = dice[1:]
-
-print(num_dice, sides_dice)
